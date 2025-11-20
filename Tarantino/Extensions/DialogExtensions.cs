8435579e13be2cddf9cfa6cddd468fdacf18a072
TimVoxel <tim.buyalski@gmail.com>
2025-11-20 18:03:17 +0100

    Added basic model and pretty printing

---
+++ Tarantino/Extensions/DialogExtensions.cs
@@ -0,0 +1,96 @@
+﻿using System.CodeDom.Compiler;
+using Tarantino.Model;
+
+namespace Tarantino.Extensions
+{
+    public static class DialogExtensions
+    {
+        public static void WriteTo(this Dialog dialog, IndentedTextWriter writer)
+        {
+            WriteDialog(dialog, writer);
+        }
+
+        public static void WriteTo(this Dialog dialog, TextWriter writer)
+        {
+            WriteDialog(dialog, new IndentedTextWriter(writer));    
+        }
+
+        private static void WriteDialog(Dialog dialog, IndentedTextWriter writer)
+        {
+            var isConsole = IsConsole(writer);
+
+            void WriteColouredIfConsoleLine(ConsoleColor color, string message)
+            {
+                if (isConsole)
+                {
+                    Console.ForegroundColor = color;
+                    writer.WriteLine(message);
+                    Console.ResetColor();
+                }
+            }
+
+            void WriteColouredIfConsole(ConsoleColor color, string message)
+            {
+                if (isConsole)
+                {
+                    Console.ForegroundColor = color;
+                    writer.Write(message);
+                    Console.ResetColor();
+                }
+            }
+
+            WriteColouredIfConsole(ConsoleColor.Blue, $"Dialog");
+            WriteColouredIfConsoleLine(ConsoleColor.Gray, ": ");
+            WriteColouredIfConsoleLine(ConsoleColor.Gray, $"\"{dialog.Text}\"");
+            writer.Indent++;
+
+            if (dialog.Responses.IsDefaultOrEmpty)
+            {
+                WriteColouredIfConsoleLine(ConsoleColor.DarkGray, "(No responses)");
+                writer.Indent--;
+                return;
+            }
+
+            WriteColouredIfConsole(ConsoleColor.Green, $"Responses");
+            WriteColouredIfConsoleLine(ConsoleColor.Gray, ": ");
+
+            writer.Indent++;
+            foreach (var response in dialog.Responses)
+            {
+                switch (response)
+                {
+                    case TextDialogResponse textResponse:
+                        WriteColouredIfConsole(ConsoleColor.Gray, "- ");
+                        WriteColouredIfConsole(ConsoleColor.Green, $"Text Response");
+                        WriteColouredIfConsole(ConsoleColor.Gray, ": ");
+                        WriteColouredIfConsoleLine(ConsoleColor.Gray, $"\"{textResponse.Text}\"");
+                        break;
+
+                    case SubDialogResponse subResponse:
+                        WriteColouredIfConsole(ConsoleColor.Gray, "- ");
+                        WriteColouredIfConsole(ConsoleColor.Green, $"Sub dialog");
+                        WriteColouredIfConsoleLine(ConsoleColor.Gray, ": ");
+                        writer.Indent++;
+                        WriteDialog(subResponse.Dialog, writer);
+                        writer.Indent--;
+                        break;
+
+                    default:
+                        throw new Exception($"Unknown response type {response.GetType()}");
+                }
+            }
+            writer.Indent--;
+        }
+
+        private static bool IsConsole(TextWriter writer)
+        {
+            if (writer == Console.Out)
+                return true;
+
+            if (writer == Console.Error)
+                return !Console.IsErrorRedirected && !Console.IsOutputRedirected;
+
+            return writer is IndentedTextWriter iw && IsConsole(iw.InnerWriter);
+        }
+    }
+}
---
+++ Tarantino/Model/Dialog.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Immutable;
+
+namespace Tarantino.Model
+{
+    public class Dialog
+    {
+        public string Text { get; }
+        public ImmutableArray<DialogResponse> Responses { get; init; }
+
+        private Dialog(string text)
+        {
+            Text = text;
+        }
+
+        public static Dialog Create(string text, params DialogResponse[] responses)
+        {
+            return new Dialog(text)
+            {
+                Responses = responses.ToImmutableArray()
+            };
+        }
+    }
+
+    public abstract class DialogResponse
+    {
+        public static TextDialogResponse Text(string text)
+        {
+            return new TextDialogResponse(text);
+        }
+
+        public static SubDialogResponse SubDialog(Dialog dialog)
+        {
+            return new SubDialogResponse(dialog);
+        }
+    }
+
+    public class TextDialogResponse : DialogResponse
+    {
+        public new string Text { get; }
+
+        public TextDialogResponse(string text)
+        {
+            Text = text;
+        }
+    }
+
+    public class SubDialogResponse : DialogResponse
+    {
+        public Dialog Dialog { get; }
+
+        public SubDialogResponse(Dialog dialog)
+        {
+            Dialog = dialog;
+        }
+    }
+}
